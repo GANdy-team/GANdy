adfe77a2346f6870e30b7b5b16d942ed5a85877d
Samantha Tetef <stetef@u.rochester.edu>
2021-03-08 18:28:07 -0800

    Added self._model in front of deepchem function calls

--- gandy/tests/test_models/test_gans.py
+++ gandy/tests/test_models/test_gans.py
@@ -31,9 +31,9 @@ class TestGAN(unittest.TestCase):
         kwargs = dict(noise_shape=(5,))
         subject._build(kwargs)
         # assert create generator function called
-        subject.create_generator.assert_called_once_with(kwargs)
+        subject._model.create_generator.assert_called_once_with(kwargs)
         # assert create discriminator function called
-        subject.create_discriminator.assert_called_once_with(kwargs)
+        subject._model.create_discriminator.assert_called_once_with(kwargs)
         # check attributes
         self.assertTrue(subject.conditional, False)
         self.assertTrue(subject.noise_shape, (5,))
@@ -45,9 +45,9 @@ class TestGAN(unittest.TestCase):
         kwargs = dict(noise_shape=(5,), n_classes=4)
         subject._build(kwargs)
         # assert create generator function called
-        subject.create_generator.assert_called_once_with(kwargs)
+        subject._model.create_generator.assert_called_once_with(kwargs)
         # assert create discriminator function called
-        subject.create_discriminator.assert_called_once_with(kwargs)
+        subject._model.create_discriminator.assert_called_once_with(kwargs)
         # check attributes
         self.assertTrue(subject.conditional, True)
         self.assertTrue(subject.noise_shape, (5,))
@@ -67,13 +67,13 @@ class TestGAN(unittest.TestCase):
         subject = gans.GAN(xshape=(4,), yshape=(2,))
         subject.iterbacthes = mock.MagicMock(name='iterbatches',
                                              return_value="Batch1")
-        subject.fit_gan = mock.MagicMock(name='fit_gan')
+        subject._model.fit_gan = mock.MagicMock(name='fit_gan')
         kwargs = dict(option='x1')
         subject._train(Xs, Ys, kwargs)
 
         # assert fit_gan was called
         subject.iterbacthes.assert_called_with(Xs, Ys, kwargs)
-        subject.fit_gan.assert_called_with("Batch1")
+        subject._model.fit_gan.assert_called_with("Batch1")
         return
 
     @unittest.mock.patch('gandy.models.gans.GAN._build', return_value='Model')
@@ -92,21 +92,21 @@ class TestGAN(unittest.TestCase):
             name='predict_gan_generator', return_value='generated_points')
         preds, ucs = subject._predict(Xs)
         subject._predict.assert_called_with(Xs)
-        subject.predict_gan_generator.assert_called_with(None)
+        subject._model.predict_gan_generator.assert_called_with(None)
         self.assertEqual('preds', 'generated_points')
         self.assertEqual('ucs', None)
 
         # CHECK Conditional GAN
         Ys = 'Ys'
         subject = gans.GAN(xshape=(4,), yshape=(2, 3), n_classes=3)
-        subject.predict_gan_generator = mock.MagicMock(
+        subject._model.predict_gan_generator = mock.MagicMock(
             name='predict_gan_generator', return_value='generated_points')
         with mock.patch('deepchem.metrics.to_one_hot',
                         return_value=[10]) as mocked_one_hot:
             preds, ucs = subject._predict(Xs, Ys=Ys)
             mocked_one_hot.assert_called_with(Ys, 3)
             subject._predict.assert_called_with(Xs, Ys=Ys)
-            subject.predict_gan_generator.assert_called_with(
+            subject._model.predict_gan_generator.assert_called_with(
                 conditional_inputs=[10])
             self.assertEqual('preds', 'generated_points')
             self.assertEqual('ucs', None)
