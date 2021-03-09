b12f5b66b93b6162eef477219f895cb21d874204
Samantha Tetef <stetef@u.rochester.edu>
2021-03-08 16:17:15 -0800

    Redistributed test functions for deepchem class versus gandy model

--- gandy/tests/test_models/test_gans.py
+++ gandy/tests/test_models/test_gans.py
@@ -1,120 +1,128 @@
-"""
-Testing functions for UncertaintyModel gan class.
-"""
+"""Testing functions for UncertaintyModel gan class."""
 
-import numpy as np
+# import numpy as np
 import unittest
-import unittest.mock
+import unittest.mock as mock
 
-import deepchem
+# import deepchem
 
 import gandy.models.gans as gans
 import gandy.models.models
 
 
-def TestGAN(unittest.test_case):
+class TestGAN(unittest.TestCase):
     """Test GAN class."""
 
     def test_inheritence():
-        # ensure the subclass class inherits from both parent classes
+        """Ensure the subclass class inherits from parent class."""
         assert issubclass(gans.gan, gandy.models.models.UncertaintyModel)
-        assert issubclass(gans.gan, deepchem.models.GAN)
-
-    def test_create_generator(self):
-        '''
-        Test create generator function.
-
-        The create generator function uses kwargs to create a Keras model.
-        This checks that the model compiles.
-        '''
-        return
-
-    def test_create_discriminator(self):
-        '''
-        Test create discriminator function.
-
-        The create discriminator function uses kwargs to create a Keras model.
-        This checks that the model compiles.
-        '''
-        return
 
     def test__build(self):
-        '''
+        """
         Test build function.
 
         The build function should create a generator and discriminator.
         This checks both functions are called. It also checks that both
         generator and discriminator are attributes with type == Keras model.
-        '''
+        """
+        # CHECK (normal) GAN
         # create gan instance
-        subject = gans.gan(xshape=(4,), yshape=(2,))
-        # create mock functions
-        subject.create_generator = mock.MagicMock(name='create_generator')
-        subject.create_discriminator = mock.MagicMock(name='create_discriminator')
-        kwargs = dict(option=x1)
-        subject.build(kwargs)
+        subject = gans.GAN(xshape=(4,), yshape=(2,))
+        kwargs = dict(noise_shape=(5,))
+        subject._build(kwargs)
         # assert create generator function called
         subject.create_generator.assert_called_once_with(kwargs)
         # assert create discriminator function called
         subject.create_discriminator.assert_called_once_with(kwargs)
         # check attributes
-        self.assertTrue(hasattr(subject, 'n_classes'))
-        self.assertTrue(hasattr(subject, 'generator'))
-        self.assertTrue(hasattr(subject, 'discriminator'))
+        self.assertTrue(hasattr('conditional', False))
+        self.assertTrue(hasattr('noise_shape', (5,)))
+
+        # CHECK Conditional GAN
+        # create gan instance
+        subject = gans.CondGAN(xshape=(4,), yshape=(2, 4))
+        kwargs = dict(noise_shape=(5,), n_classes=4)
+        subject._build(kwargs)
+        # assert create generator function called
+        subject.create_generator.assert_called_once_with(kwargs)
+        # assert create discriminator function called
+        subject.create_discriminator.assert_called_once_with(kwargs)
+        # check attributes
+        self.assertTrue(hasattr('conditional', True))
+        self.assertTrue(hasattr('noise_shape', (5,)))
+        self.assertTrue(hasattr('n_classes', 4))
         return
 
     def test__train(self):
-        '''
+        """
         Test train function.
 
         The train function calls the fit function for the generator and the
         predict function for the discriminator.
         This checks that there is a Keras callback History object returned.
-        '''
+        """
         Xs = 'Xs'
         Ys = 'Ys'
-        subject = gans.gan(xshape=(4,), yshape=(2,))
-        kwargs = dict(option=x1)
+        subject = gans.GAN(xshape=(4,), yshape=(2,))
+        subject.iterbacthes = mock.MagicMock(name='iterbatches',
+                                             return_value="Batch1")
+        subject.fit_gan = mock.MagicMock(name='fit_gan')
+        kwargs = dict(option='x1')
         subject._train(Xs, Ys, kwargs)
 
-        
-        # assert generator model called fit and discriminator called predict
-        subject.generator.fit.assert_called_with(Xs, Ys, kwargs)
-        subject.discriminator.predict.assert_called_with(Xs, Ys, kwargs)
+        # assert fit_gan was called
+        subject.iterbacthes.assert_called_with(Xs, Ys, kwargs)
+        subject.fit_gan.assert_called_with("Batch1")
         return
 
-    def test__predict(self):
-        '''
+    @unittest.mock.patch('gandy.models.gans.GAN._build', return_value='Model')
+    def test__predict(self, mocked__build):
+        """
         Test predict function.
 
         The predict function returns predictions and uncertainties.
         This checks predictions and uncertainties are the appropriate shape
         and the appropriate deepchem calls are made.
-        '''
+        """
         Xs = 'Xs'
+        # CHECK (normal) GAN
         subject = gans.gan(xshape=(4,), yshape=(2,))
-
-        subject.predict_gan_generator = mock.MagicMock(name='predict_gan_generator')
-
-        subject._predict.return_value = ('preds', 'ucs')
+        subject.predict_gan_generator = mock.MagicMock(
+            name='predict_gan_generator', return_value='generated_points')
         preds, ucs = subject._predict(Xs)
         subject._predict.assert_called_with(Xs)
-        self.assertEqual('preds', preds)
-        self.assertEqual('ucs', ucs)
+        subject.predict_gan_generator.assert_called_with(None)
+        self.assertEqual('preds', 'generated_points')
+        self.assertEqual('ucs', None)
+
+        # CHECK Conditional GAN
+        Ys = 'Ys'
+        subject = gans.gan(xshape=(4,), yshape=(2, 3), n_classes=3)
+        subject.predict_gan_generator = mock.MagicMock(
+            name='predict_gan_generator', return_value='generated_points')
+        with mock.path('deepchem.metrics.to_one_hot',
+                       return_value=[10]) as mocked_one_hot:
+            preds, ucs = subject._predict(Xs, Ys=Ys)
+            mocked_one_hot.assert_called_with(Ys, 3)
+            subject._predict.assert_called_with(Xs, Ys=Ys)
+            subject.predict_gan_generator.assert_called_with(
+                conditional_inputs=[10])
+            self.assertEqual('preds', 'generated_points')
+            self.assertEqual('ucs', None)
         return
 
     def test__save(self):
-        '''
+        """
         Test save function.
 
         This checks that a file is written with the appropriate name.
-        '''
+        """
         return
 
     def test__load(self):
-        '''
+        """
         Test load function.
 
         This checks that a Keras model instance is returned.
-        '''
+        """
         return
